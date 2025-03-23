637299404f5706bca797e653681011ccb4dfe1d0
aaaaxiaoxu <Liwei.Xu22@student.xjtlu.edu.cn>
2025-03-23 17:11:54 +0800

    First conmit

---
+++ CPT202_frontend/src/components/Globalheader.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="globalHeader">
+    <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import { h, ref } from 'vue'
+import { HomeOutlined } from '@ant-design/icons-vue'
+import { MenuProps } from 'ant-design-vue'
+
+const current = ref<string[]>(['home'])
+const items = ref<MenuProps['items']>([
+  {
+    key: '/',
+    icon: () => h(HomeOutlined),
+    label: '主页',
+    title: '主页',
+  },
+  {
+    key: '/about',
+    label: '吴云峰小牛',
+    title: '吴云峰',
+  },
+  {
+    key: 'others',
+    label: '吴云峰大牛',
+    title: '吴云峰',
+  },
+])
+</script>
+
+
